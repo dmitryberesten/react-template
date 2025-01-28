882d594b4ef50ea8c1b535fa50dd0b617a48a169
Dima Luzhny <117902450+AmidQ1@users.noreply.github.com>
2025-01-28 18:33:58 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Example from './components/example/Example'
+import Qwert from './components/example/Qwert'
+import Card from './components/example/Card'
 
 export const App = () => {
   return (
     <div>
       <Example />
+      <Qwert />
+      <Card />
     </div>
   );
 };
---
+++ src/components/example/Qwert.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Qwert = () => {
+  return (
+      <p>Hello</p>
+  );
+};
+
+export default Qwert;
