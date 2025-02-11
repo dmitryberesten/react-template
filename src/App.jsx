f9233cba38852b31578c786a2f163a068e4bd9ae
Dima Luzhny <117902450+AmidQ1@users.noreply.github.com>
2025-02-11 20:09:55 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Recipe } from './components/Recipe';
-import recipes from './recipes.json';
-import Recipe from './components/example/Recipe';
-import RecipeInfo from './components/example/RecipeInfo';
+import { Recipe } from './components/example/Recipe';
+import recipes from '../recipes.json';
+import'App.css'
+
 
 export const App = () => {
   return (
     <div>
-      <h1>Recipe List</h1>
+      <h1 class="title">Recipe List</h1>
       {recipes.map((recipe, index) => (
         <Recipe key={index} recipe={recipe} />
       ))}
